74f967ab722ae1cbc1b1d8c5e58fe816283090e7
N3TH1K4 <88619598+N3TH1K4@users.noreply.github.com>
2022-07-18 08:07:13 +0530

    Add files via upload

---
+++ Config.py
@@ -0,0 +1,61 @@
+# (c) @Friend_A_Kousei
+
+import os
+
+
+
+API_ID = 18983092
+API_HASH = "a6005a70e88369b4fb08b8350ebbdd35"
+BOT_TOKEN = "5420032037:AAHxg-kuGXULTuhTAgSKIEmwt9USXmL1qIU"
+SESSION_NAME ="Ichika"
+SLEEP_TIME =  0
+BOT_OWNER = 1930645496
+CAPTION = ""
+UPDATES_CHANNEL = None
+LOG_CHANNEL = -1001558732171
+MONGODB_URI = "mongodb+srv://TROJ3N:Nethika123@cluster0.uppg6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+DOWNLOAD_PATH = "./downloads"
+BROADCAST_AS_COPY = True
+ONE_PROCESS_ONLY = False
+START_TEXT ="""
+Hi!! 
+I'm **Ichigo** A Simple File Rename Bot With Permenant Thumbnail Support💯.
+
+__For More Details Check Help__📜
+"""
+HELP_TEXT = """**Follow these Steps For Using Me..**
+
+➠ Configure the Settings before using me.....
+➠ Send a photo to set it as your custom thumbnail..... 
+➠ Send any File or media you want to rename..... 
+➠ That's it, and rest is mine work..... 
+
+📝 Available Commands 📝
+- /start - Start the Bot
+- /help - How to Use
+- /about - About Me
+- /settings - Configure Settings 
+- /showthumb & /delthumb - For Thumbnail
+
+__⭕️Note: Multitasking supported, but only do two tasks at once, or the bot may crash..__
+"""
+ABOUT_TEXT = """
+📌 A Telegram Bot Can Help You For Renaming File/Video/Audio With Permenant Thumbnail Support💯 Copy Of @SakuraRenamer_bot.       
+
+📝**Language:** [Python3](https://www.python.org)
+
+📚**Library:** [Pyrogram](https://docs.pyrogram.org)
+
+📡**Hosted On:** [Heorku](https://heroku.com)
+
+👨‍💻**Dev:** [TROJ3N](https://t.me/troj3n)
+"""
+PROGRESS = """
+**📊Progress:** {0}%
+
+**✅Done:** {1} **Of** {2}
+
+**🚀Speed:** {3}/s
+
+**⏳ETA:** {4}
+"""
