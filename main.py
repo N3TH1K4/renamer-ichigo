75b8af6037bda8901e110fd7a66f0e8fa2faedcf
N3TH1K4 <88619598+N3TH1K4@users.noreply.github.com>
2022-07-18 08:29:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -104,7 +104,9 @@ async def rename_handler(bot: Client, event: Message):
             quote=True
         )
         lol =  media.file_name
+	print(lol)
         newf = lol.replace("@Anime_Gallery", "[ON]")
+	print(newf)
         if "720p" in lol:
             qua = "[720p]"
         elif "1080p" in lol:
@@ -125,7 +127,7 @@ async def rename_handler(bot: Client, event: Message):
             if ask_.text and (ask_.text.startswith("/") is False):
                 ascii_ = ''.join([i if (i in string.digits or i in string.ascii_letters or i == "_", "_") else "" for i in ask_.text.rsplit('_', 1)[0]])
                 prefix_ = await db.get_prefix(event.from_user.id)
-                new_file_name = f"{download_location}{prefix_ if (prefix_ is not None) else ''}{ascii_.replace('.', '.')} {qua} {mod} [ON].{media.file_name.rsplit('.', 1)[-1]}"
+                new_file_name = f"{download_location}{prefix_ if (prefix_ is not None) else ''}{ascii_.replace('.', '.')}.{media.file_name.rsplit('.', 1)[-1]}"
                 if len(new_file_name) > 255:
                     await reply_.edit("Sorry Unkil,\nFile Name length is more than 255 bytes!")
                     return
